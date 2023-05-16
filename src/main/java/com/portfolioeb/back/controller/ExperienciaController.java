244f14c76eadf92646b7b2d3a8005beacbccfdc1
Emiliano Broggi <broggi4@hotmail.com>
2023-05-15 22:45:37 -0300

    exp lab

--- src/main/java/com/portfolioeb/back/controller/ExperienciaController.java
+++ src/main/java/com/portfolioeb/back/controller/ExperienciaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("explab")
+@RequestMapping("/explab")
 @CrossOrigin(origins = "https://portfolio-emilianobroggi.web.app")
 
 public class ExperienciaController {
